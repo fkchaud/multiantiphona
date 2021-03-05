74c5f335401e7eb512e9cd57db5f3fb699ef9e01
Facundo Chaud <eye.imper@gmail.com>
2021-03-05 18:20:53 -0300

    Add antiphona app

---
+++ antiphona/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AntiphonaConfig(AppConfig):
+    name = 'antiphona'
---
+++ antiphona/views.py
@@ -0,0 +1,8 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello, world. You're at the polls index.")
